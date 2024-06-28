e0e78ddb7b5da608ab1c3061a9453740bc1e0b18
PiyapatWaw <Piyapat.Waw@outlook.com>
2024-06-28 12:16:25 +0700

    update to unity 2021

---
+++ Assets/script/splashscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splashscene : MonoBehaviour
+{
+    public GameObject Logo;
+    public GameObject TransitionPic;
+    
+    IEnumerator Start () 
+    {
+        Logo.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1.5f);
+        TransitionPic.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
